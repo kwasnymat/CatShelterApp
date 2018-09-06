0da082a2f94a368df1dc47a7115d5c2b5baddf61
kwasnymat <kwasnymat@gmail.com>
2018-09-06 21:27:41 +0200

    gulpfile working

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
 
-gulp.task('scss', function() {
-    return gulp.src('sass/style.scss')
+gulp.task('sass', function() {
+    return gulp.src('scss/style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass ({
             errLogConsole: true,
@@ -25,9 +25,9 @@ gulp.task('scss', function() {
 			}))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('css'))
-})
+});
 
 
-gulp.task('default', ['scss'], function() {
-    gulp.watch('sass/main.scss', ['scss'])
+gulp.task('watch', function () {
+    gulp.watch('scss/**/*.scss', ['sass']);
 });
